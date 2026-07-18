4b525beeb42a644a731dee6c94f7c810aca29c5a
Raghav Ahuja <raghavahuja2801@gmail.com>
2026-07-18 18:24:54 +0530

    sprint 1 finished, added the basic terraform code

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+

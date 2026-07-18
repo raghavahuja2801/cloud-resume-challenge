357480a4b5160322a50914910f00c5faa580a56b
Raghav Ahuja <raghavahuja2801@gmail.com>
2026-07-19 02:38:10 +0530

    updated outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,3 +10,13 @@ output "api_url" {
   value = module.apigateway.api_endpoint
 }
 
+output "bucket_name" {
+  description = "S3 website bucket name"
+  value       = module.s3.bucket_name
+}
+
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID"
+  value       = module.cloudfront.distribution_id
+}
+
